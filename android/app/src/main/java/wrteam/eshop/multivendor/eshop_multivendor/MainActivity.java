0558b52bf492cc7a30f1b6671c057130a0a35d82
Alphawizz Flutter Team <alphawizzflutterteam@gmail.com>
2024-08-16 13:50:38 +0530

    first commit

---
+++ android/app/src/main/java/wrteam/eshop/multivendor/eshop_multivendor/MainActivity.java
@@ -0,0 +1,6 @@
+package com.aquakart.user;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,22 @@
+final String appName = "Aquakart";
+
+final String packageName = "com.aquakart.user";
+final String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+final String iosPackage = "com.aquakart.user";
+final String iosLink = 'your ios link here';
+final String appStoreId = "com.aquakart.user";
+
+final String deepLinkUrlPrefix = 'https://alpha.ecommerce.link';
+final String deepLinkName = 'alpha.ecommerce.link';
+
+final int timeOut = 50;
+const int perPage = 10;
+
+// final String baseUrl = 'https://buransmart.in/app/v1/api/';
+// final String baseUrl = 'https://alphawizzserver.com/aquakart/app/v1/api/';
+final String baseUrl = 'https://aquakartindia.com/app/v1/api/';
+
+final String imageUrl = 'https://aquakartindia.com/';
+
+final String jwtKey = "e1e421dceee9ea718ab764ea4fdef95629e7f415";
